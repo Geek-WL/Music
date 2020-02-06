7ea21e4e0526cdaf0f9bf8281f84cc840fc25f90
MrLohh <6149878334@qq.com>
2020-02-06 21:30:14 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div class="t"></div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+  .t {
+    width: 100px;
+    height: 100px;
+    background: red;
+  }
+</style>
